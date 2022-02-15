431f8148fdbde074b0538ec6ca95cd2f52fd0734
B Steele <steeleb@caryinstitute.org>
2022-02-15 15:31:43 -0700

    update to CV

--- sunapee_hobodo_2018.R
+++ sunapee_hobodo_2018.R
@@ -1,12 +1,14 @@
 source('C:/Users/steeleb/Dropbox/Lake Sunapee/monitoring/buoy data/programs/library_func_lists.R')
 
 #### HOBO DO ####
+
+hobo_tz = 'America/New_York'
 hobo_updo <- read_csv('C:/Users/steeleb/Dropbox/Lake Sunapee/monitoring/buoy data/data/all sensors/L0/2018_Summer_U26_DO.csv',
                       skip = 2,
                       col_types = c('icnnccccc'),
                       col_names = c('x', 'datetime', 'do_mgl', 'temp_degC', 'x2', 'x3', 'x4', 'x5', 'x6')) %>% 
   select(datetime, do_mgl, temp_degC) %>% 
-  mutate(datetime = as.POSIXct(datetime, tz='UTC', format = '%m/%d/%y %I:%M:%S %p'))
+  mutate(datetime = as.POSIXct(datetime, tz=hobo_tz, format = '%m/%d/%y %I:%M:%S %p'))
 
 hobo_updo_vert <- hobo_updo %>% 
   gather(variable, value, -datetime)
@@ -21,7 +23,7 @@ hobo_updo_vert <- hobo_updo %>%
 #   scale_color_colorblind()
 # 
 # ggplot(subset(hobo_updo_vert,
-#               (subset = datetime >= as.POSIXct('2018-10-01', tz='UTC'))),
+#               (subset = datetime >= as.POSIXct('2018-10-01', tz=hobo_tz))),
 #        aes(x=datetime, y=value)) +
 #   geom_point() +
 #   facet_grid(variable ~ ., scales = 'free_y') +
@@ -32,7 +34,7 @@ hobo_updo_vert <- hobo_updo %>%
 # 
 # #sensor removed oct 12
 # ggplot(subset(hobo_updo_vert,
-#               (subset = datetime >= as.POSIXct('2018-10-12', tz='UTC'))),
+#               (subset = datetime >= as.POSIXct('2018-10-12', tz=hobo_tz))),
 #        aes(x=datetime, y=value)) +
 #   geom_point() +
 #   facet_grid(variable ~ ., scales = 'free_y') +
@@ -43,9 +45,9 @@ hobo_updo_vert <- hobo_updo %>%
 
 hobo_updo_L1 <- hobo_updo %>% 
   mutate_at(vars(do_mgl, temp_degC),
-            funs(case_when(datetime >= as.POSIXct('2018-10-12 10:30', tz='UTC') ~ NA_real_,
+            funs(case_when(datetime >= as.POSIXct('2018-10-12 10:30', tz=hobo_tz) ~ NA_real_,
                            TRUE ~ .))) %>% 
-  mutate(hobodo_flag = case_when(datetime == as.POSIXct('2018-05-23 12:00:00', tz='UTC') ~ 'c',
+  mutate(hobodo_flag = case_when(datetime == as.POSIXct('2018-05-23 12:00:00', tz=hobo_tz) ~ 'c',
                                TRUE ~ ''))
 
 hobo_updo_vert_L1 <- hobo_updo_L1 %>% 
@@ -60,9 +62,22 @@ hobo_updo_vert_L1 <- hobo_updo_L1 %>%
 #   final_theme +
 #   scale_color_colorblind()
 
+# add CV
+colnames(hobo_updo_L1)
+hobo_updo_L1 <- hobo_updo_L1 %>% 
+  rename(datetime.local = datetime,
+         oxygenDissolved_mgl_0p25m = do_mgl,
+         waterTemperature_degC_0p25m = temp_degC,
+         flag_do0p25m = hobodo_flag)
+
+#convert time to UTC-5
+hobo_updo_L1 <- hobo_updo_L1 %>% 
+  mutate(datetime = with_tz(datetime.local, tz = 'Etc/GMT+5'))
+
 #export l1 hobo do file
 hobo_updo_L1 %>%
+  select(-datetime.local) %>% 
   mutate(datetime = as.character(datetime)) %>%
   mutate(location = 'loon') %>% 
-  write_csv(., 'C:/Users/steeleb/Dropbox/Lake Sunapee/monitoring/buoy data/data/all sensors/L1/do/2018_hobodo_L1.csv') %>% 
-  write_csv(., 'C:/Users/steeleb/Dropbox/Lake Sunapee/monitoring/buoy data/data/all sensors/L1/record collations/do/2018_hobodo_L1.csv')
+  write_csv(., 'C:/Users/steeleb/Dropbox/Lake Sunapee/monitoring/buoy data/data/all sensors/L1/do/2018_hobodo_L1_v2022.csv')
+  
